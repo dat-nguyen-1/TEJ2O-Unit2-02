e3390a8ab07c1c1ee907710562e283c66c7dd2b1
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-02-13 18:12:56 +0000

    update docstring and change to show "Hello, MTHS!"

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,9 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Dat Nguyen
+Created on: Feb 2026
+This module shows "Hello, MTHS!" on the Micro:Bit's screen.
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+display.scroll("Hello, MTHS!")
