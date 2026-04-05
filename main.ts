9ad0311e14157f89b1985b4cd984e5ca94496972
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-04-05 16:44:01 -0400

    Fix comment formatting and update display string

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Dat Nguyen
- * Created on: Feb 2026
- * This program shows "Hello, MTHS!"
+*
+* Created by: Dat Nguyen
+* Created on: Feb 2026
+* This program shows "Hello, MTHS!"
 */
 
-basic.showString('Hello, MTHS!')
+// display "Hello, MTHS!"
+basic.showString("Hello, MTHS!")
